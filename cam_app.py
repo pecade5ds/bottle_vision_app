cb1dd55959a21bc08a3a755085b63c187f2b9e99
Pecade <pedro.cadahia@5digitalstreet.com>
2024-11-28 22:31:34 +0100

    Update cam_app.py

--- cam_app.py
+++ cam_app.py
@@ -109,8 +109,8 @@ def main():
 
         # Predict using YOLO model
         #################################
-        # roboflow_result = yolo_models_dict["roboflow_model"].predict(np.array(Image.open(picture)) , confidence=50, overlap=30)
-        # robo_detected_label_counts_dict = filter_and_count(roboflow_result.json()["predictions"], threshold=0.5, class_var="class")
+        roboflow_result = yolo_models_dict["roboflow_model"].predict(np.array(Image.open(picture)) , confidence=50, overlap=30)
+        robo_detected_label_counts_dict = filter_and_count(roboflow_result.json()["predictions"], threshold=0.5, class_var="class")
 
         # Base model for Bottle detection (denominator for computing "Water store share")
         #################################
