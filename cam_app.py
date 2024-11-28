9129fd9aea6b24b2fb776182a9309822f1729a13
Pecade <pedro.cadahia@5digitalstreet.com>
2024-11-28 23:02:47 +0100

    Update cam_app.py

--- cam_app.py
+++ cam_app.py
@@ -11,6 +11,7 @@ from PIL import Image
 from roboflow import Roboflow
 from google.cloud import firestore
 from utils import *
+import pandas as pd
 
 # Firebase credentials
 db = firestore.Client.from_service_account_info(st.secrets["firebase"])
