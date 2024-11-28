81e4c53b4f040b7fea4336f7417912c43cf5c4b4
Pecade <pedro.cadahia@5digitalstreet.com>
2024-11-28 22:11:07 +0100

    Update cam_app.py

--- cam_app.py
+++ cam_app.py
@@ -87,8 +87,7 @@ def main():
         enable_camera = st.checkbox("Enable Camera")
 
         # Widget to capture the photo
-        picture = st.camera_input("Take a photo", disabled=not enable_camera).read()
-
+        picture = st.camera_input("Take a photo", disabled=not enable_camera)
     elif mode == "Upload a Photo":
         # Widget to upload a photo
         picture = st.file_uploader("Upload a photo", type=["jpg", "jpeg", "png"])
